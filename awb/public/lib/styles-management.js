ac6a0d991f412ffdb45d4de4b215364a393ea73c
rafid-al-mahmud <rafidalmahmudper09@gmail.com>
2022-11-14 16:07:02 +0600

    .

---
+++ awb/public/lib/styles-management.js
@@ -0,0 +1,519 @@
+const styles_list = {
+    styles: [
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "Align Content",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-items",
+            class_name: "alignItems",
+            display_name: "Align Items",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "Align Content",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+        {
+            name: "align-content",
+            class_name: "alignContent",
+            display_name: "alignContent",
+            editBox: {
+                type: "button",
+                button: ["stretch","center","flex-start","flex-end","space-between","space-around","initial","inherit"]
+            }
+        },
+    ]
+}
+styles_list.styles[0]
+console.log(styles_list.styles)
